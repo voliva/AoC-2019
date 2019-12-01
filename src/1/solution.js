20261b5ae2438d455b8c501437692562faaface5
Víctor Oliva <olivarra1@gmail.com>
2019-12-01 06:07:16 +0100

    Solution to day 1

---
+++ src/1/solution.js
@@ -0,0 +1,23 @@
+const getFuel = v => {
+  const value = Math.floor(v / 3) - 2;
+  if(value < 0) {
+    return 0;
+  }
+  return value + getFuel(value);
+}
+
+const solution1 = inputLines => {
+  return inputLines
+    .map(v => Number(v))
+    .map(v => Math.floor(v / 3) - 2)
+    .reduce((a, b) => a + b);
+};
+
+const solution2 = inputLines => {
+  return inputLines
+    .map(v => Number(v))
+    .map(v => getFuel(v))
+    .reduce((a, b) => a + b);
+};
+
+module.exports = [solution1, solution2];
