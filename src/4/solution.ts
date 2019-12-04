8128aae7a16731e29fefb40d1f97574ba1a7a23a
Víctor Oliva <olivarra1@gmail.com>
2019-12-04 09:18:36 +0100

    Part 2 spin off

--- src/4/solution.ts
+++ src/4/solution.ts
@@ -34,31 +34,34 @@ const solution2 = (inputLines: string[]) => {
     const chars = String(psw)
       .split('')
       .map(Number);
-    const has2Adjacent =
-      chars.reduce(
-        (acc, char) => {
-          if (acc.group === char) {
-            return {
-              group: acc.group,
-              count: acc.count + 1,
-            };
-          }
-          if (acc.count === 2) {
-            return {
-              group: null,
-              count: 2,
-            };
-          }
+    const result = chars.reduce(
+      (acc, char) => {
+        if (acc.group === char) {
           return {
-            group: char,
-            count: 1,
+            ...acc,
+            count: acc.count + 1,
           };
-        },
-        {
-          group: null as null | number,
-          count: 0,
         }
-      ).count === 2;
+        if (acc.count > 2 && acc.count % 2 !== 0) {
+          return {
+            ...acc,
+            group: null,
+          };
+        }
+        return {
+          group: char,
+          count: 1,
+          shadowedAdjacent: acc.shadowedAdjacent || acc.count >= 2,
+        };
+      },
+      {
+        group: null as null | number,
+        count: 0,
+        shadowedAdjacent: false,
+      }
+    );
+    const has2Adjacent =
+      result.count >= 2 ? result.count % 2 === 0 : result.shadowedAdjacent;
     const neverDecreases =
       chars.findIndex(
         (char, index, arr) => index > 0 && arr[index - 1] > char
