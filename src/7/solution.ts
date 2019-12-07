eef1f7c13df15722f8805b6b6394918ddd9ed4d7
Víctor Oliva <olivarra1@gmail.com>
2019-12-07 17:09:25 +0100

    Day 7 cleanup

--- src/7/solution.ts
+++ src/7/solution.ts
@@ -1,5 +1,110 @@
 declare const process: any;
 
+const solution1 = (inputLines: string[]) => {
+  const program = inputLines[0].split(',').map(Number);
+
+  const signals = [0, 1, 2, 3, 4];
+  let max = 0;
+
+  loopParameters(signals, 5, parameters => {
+    const output = parameters.reduce((previousOut, param) => {
+      const inputIterator = [param, previousOut][Symbol.iterator]();
+      const solution = solve(0, program, inputIterator).next();
+
+      if (solution.done) {
+        throw new Error('Program did not yield an output');
+      }
+      return solution.value;
+    }, 0);
+
+    max = Math.max(output, max);
+  });
+
+  return max;
+};
+
+const loopParameters = (
+  values: number[],
+  n: number,
+  fn: (parameters: number[]) => void
+) => {
+  const totalPerm = Math.pow(n, values.length);
+  for (let i = 0; i < totalPerm; i++) {
+    const parameters = new Array(n).fill(0).map((_, idx) => {
+      const shift = Math.floor(i / Math.pow(values.length, idx));
+      return values[shift % values.length];
+    });
+
+    const paramSet = new Set(parameters);
+    if (paramSet.size < parameters.length) {
+      continue;
+    }
+
+    fn(parameters);
+  }
+};
+
+function* solve(id: number, program: number[], input: Iterator<number, null>) {
+  let mem = [...program];
+
+  let ip = 0;
+  let opcode: ReturnType<typeof readOpcode>;
+  while ((opcode = readOpcode(mem[ip])).operation !== 99) {
+    const a = opcode.modeA === 0 ? mem[mem[ip + 1]] : mem[ip + 1];
+    const b = opcode.modeB === 0 ? mem[mem[ip + 2]] : mem[ip + 2];
+    const dest = mem[ip + 3];
+
+    switch (opcode.operation) {
+      case 1:
+        mem[dest] = a + b;
+        ip += 4;
+        break;
+      case 2:
+        mem[dest] = a * b;
+        ip += 4;
+        break;
+      case 3:
+        const next = input.next();
+        if (next.done) {
+          throw new Error(id + ' trying to read but input is empty');
+        }
+        mem[mem[ip + 1]] = next.value;
+        ip += 2;
+        break;
+      case 4:
+        yield a;
+        ip += 2;
+        break;
+      case 5:
+        if (a !== 0) {
+          ip = b;
+        } else {
+          ip += 3;
+        }
+        break;
+      case 6:
+        if (a === 0) {
+          ip = b;
+        } else {
+          ip += 3;
+        }
+        break;
+      case 7:
+        mem[dest] = a < b ? 1 : 0;
+        ip += 4;
+        break;
+      case 8:
+        mem[dest] = a === b ? 1 : 0;
+        ip += 4;
+        break;
+      default:
+        throw new Error('unknown opcode ' + JSON.stringify(opcode));
+    }
+  }
+
+  return null;
+}
+
 const readOpcode = opcode => {
   const operation = opcode % 100;
   opcode = Math.floor(opcode / 100);
@@ -8,6 +113,7 @@ const readOpcode = opcode => {
   const modeB = opcode % 10;
   opcode = Math.floor(opcode / 10);
   const modeC = opcode % 10;
+
   return {
     operation,
     modeA,
@@ -16,171 +122,43 @@ const readOpcode = opcode => {
   };
 };
 
-const solve = (program, inputs) => {
-  let mem = [...program];
-
-  let inputPointer = 0;
-  let pc = 0;
-  let opcode: ReturnType<typeof readOpcode>;
-  const outputs: number[] = [];
-  while ((opcode = readOpcode(mem[pc])).operation !== 99) {
-    const a = opcode.modeA === 0 ? mem[mem[pc + 1]] : mem[pc + 1];
-    const b = opcode.modeB === 0 ? mem[mem[pc + 2]] : mem[pc + 2];
-    const dest = mem[pc + 3];
-    console.log(opcode, a, b, dest);
-    switch (opcode.operation) {
-      case 1:
-        mem[dest] = a + b;
-        pc += 4;
-        break;
-      case 2:
-        mem[dest] = a * b;
-        pc += 4;
-        break;
-      case 3:
-        mem[mem[pc + 1]] = inputs[inputPointer];
-        console.log(mem[pc + 1], mem[mem[pc + 1]]);
-        inputPointer++;
-        pc += 2;
-        break;
-      case 4:
-        outputs.push(a);
-        pc += 2;
-        break;
-      case 5:
-        if (a !== 0) {
-          pc = b;
-        } else {
-          pc += 3;
-        }
-        break;
-      case 6:
-        if (a === 0) {
-          pc = b;
-        } else {
-          pc += 3;
-        }
-        break;
-      case 7:
-        mem[dest] = a < b ? 1 : 0;
-        pc += 4;
-        break;
-      case 8:
-        mem[dest] = a === b ? 1 : 0;
-        pc += 4;
-        break;
-      default:
-        return null;
-    }
-  }
-
-  process.exit(1);
-
-  return outputs;
-};
-
-const solution1 = (inputLines: string[]) => {
+const solution2 = (inputLines: string[]) => {
   const program = inputLines[0].split(',').map(Number);
 
-  const signals = [0, 1, 2, 3, 4];
-  const amplifiers = ['A', 'B', 'C', 'D', 'E'];
-  const totalPerm = Math.pow(amplifiers.length, signals.length);
+  const signals = [5, 6, 7, 8, 9];
   let max = 0;
-  for (let i = 0; i < totalPerm; i++) {
-    const parameters = amplifiers.map((_, idx) => {
-      const shift = Math.floor(i / Math.pow(signals.length, idx));
-      return signals[shift % signals.length];
+
+  loopParameters(signals, 5, parameters => {
+    const inputIterator = createInputIterator();
+    inputIterator.push({
+      value: 0,
     });
 
-    const paramSet = new Set(parameters);
-    if (paramSet.size < parameters.length) {
-      continue;
+    const amplifiers: IterableIterator<number>[] = [];
+    for (let i = 0; i < parameters.length; i++) {
+      const input = prependIterator(
+        i === 0 ? inputIterator : amplifiers[i - 1],
+        parameters[i]
+      );
+      const amplifier = solve(i, program, input);
+      amplifiers.push(amplifier);
     }
 
-    const output = parameters.reduce((acc, param) => {
-      const solution = solve(program, [param, acc]);
-      if (!solution || solution.length > 1) {
-        throw new Error('unexpected output ' + (solution && solution.length));
-      }
-      return solution[0];
-    }, 0);
-    console.log(`${i}/${totalPerm}`, parameters, output);
-    max = Math.max(output, max);
-  }
+    const lastAmplifier = amplifiers[amplifiers.length - 1];
+
+    let lastValue: number = 0;
+    for (let result of lastAmplifier) {
+      inputIterator.push({
+        value: result,
+      });
+      lastValue = result;
+    }
+
+    max = Math.max(lastValue, max);
+  });
 
   return max;
 };
-
-function* amplifier(
-  id: number,
-  program: number[],
-  inputs: Iterator<number, null>
-) {
-  let mem = [...program];
-
-  const logIO = (...args: any) => {};
-
-  let pc = 0;
-  let opcode: ReturnType<typeof readOpcode>;
-  while ((opcode = readOpcode(mem[pc])).operation !== 99) {
-    const a = opcode.modeA === 0 ? mem[mem[pc + 1]] : mem[pc + 1];
-    const b = opcode.modeB === 0 ? mem[mem[pc + 2]] : mem[pc + 2];
-    const dest = mem[pc + 3];
-    switch (opcode.operation) {
-      case 1:
-        mem[dest] = a + b;
-        pc += 4;
-        break;
-      case 2:
-        mem[dest] = a * b;
-        pc += 4;
-        break;
-      case 3:
-        logIO(id, 'read');
-        const next = inputs.next();
-        logIO(id, 'read', next);
-        if (!next.done) {
-          mem[mem[pc + 1]] = next.value;
-        } else {
-          throw new Error('trying to read but no more inputs');
-        }
-        pc += 2;
-        break;
-      case 4:
-        logIO(id, 'yield', a);
-        yield a;
-        pc += 2;
-        break;
-      case 5:
-        if (a !== 0) {
-          pc = b;
-        } else {
-          pc += 3;
-        }
-        break;
-      case 6:
-        if (a === 0) {
-          pc = b;
-        } else {
-          pc += 3;
-        }
-        break;
-      case 7:
-        mem[dest] = a < b ? 1 : 0;
-        pc += 4;
-        break;
-      case 8:
-        mem[dest] = a === b ? 1 : 0;
-        pc += 4;
-        break;
-      default:
-        throw new Error('unknown opcode ' + JSON.stringify(opcode));
-    }
-  }
-
-  logIO(id, 'halt');
-  return null;
-}
 
 const createInputIterator = () => {
   let values: IteratorResult<number, null>[] = [];
@@ -188,11 +166,10 @@ const createInputIterator = () => {
 
   return {
     next: () => {
-      // console.log(values);
       const value = values[i];
       i++;
       if (!value) {
-        throw new Error('read out of bounds');
+        throw new Error("InputIterator - Trying to read but it's empty");
       }
       return value;
     },
@@ -213,56 +190,6 @@ const prependIterator = <T>(iterator: Iterator<T, null>, value: T) => {
       };
     },
   };
-};
-
-const solution2 = (inputLines: string[]) => {
-  const program = inputLines[0].split(',').map(Number);
-
-  const signals = [5, 6, 7, 8, 9];
-
-  const totalAmplifiers = 5;
-  const totalPerm = Math.pow(totalAmplifiers, signals.length);
-  let max = 0;
-  for (let i = 0; i < totalPerm; i++) {
-    const parameters = new Array(totalAmplifiers).fill(0).map((_, idx) => {
-      const shift = Math.floor(i / Math.pow(signals.length, idx));
-      return signals[shift % signals.length];
-    });
-
-    const paramSet = new Set(parameters);
-    if (paramSet.size < parameters.length) {
-      continue;
-    }
-
-    const inputIterator = createInputIterator();
-    inputIterator.push({
-      value: 0,
-    });
-
-    const amplifiers: IterableIterator<number>[] = [];
-    for (let i = 0; i < totalAmplifiers; i++) {
-      const input = prependIterator(
-        i === 0 ? inputIterator : amplifiers[i - 1],
-        parameters[i]
-      );
-      amplifiers.push(amplifier(i, program, input));
-    }
-
-    const outputAmplifier = amplifiers[amplifiers.length - 1];
-
-    let lastValue: number = 0;
-    for (let result of outputAmplifier) {
-      inputIterator.push({
-        value: result,
-      });
-      lastValue = result;
-    }
-
-    max = Math.max(lastValue, max);
-    console.log(`${i}/${totalPerm}`, parameters, max, lastValue);
-  }
-
-  return max;
 };
 
 export {solution1, solution2};
