e6a08dafc43da642068eb63660a75cff48efc120
Víctor Oliva <olivarra1@gmail.com>
2019-12-14 11:26:36 +0100

    Efficient solution to day 14

--- src/14/solution.ts
+++ src/14/solution.ts
@@ -38,17 +38,12 @@ const solution1 = (inputLines: string[]) => {
     const produceQty = Math.max(0, quantity - leftOverQty);
     const timesNeeded = Math.ceil(produceQty / reaction.quantity);
     const leftOver = timesNeeded * reaction.quantity - produceQty;
-    const result = (() => {
-      let total = 0;
-      for (let i = 0; i < timesNeeded; i++) {
-        total += reaction.needs.reduce(
-          (total, part) =>
-            total + getOreNeeded(part.chemical, part.quantity, leftOvers),
-          0
-        );
-      }
-      return total;
-    })();
+    const result = reaction.needs.reduce(
+      (total, part) =>
+        total +
+        getOreNeeded(part.chemical, timesNeeded * part.quantity, leftOvers),
+      0
+    );
 
     if (leftOverQty < quantity) {
       leftOvers.set(chemical, leftOver);
@@ -82,17 +77,12 @@ const solution2 = (inputLines: string[]) => {
     const produceQty = Math.max(0, quantity - leftOverQty);
     const timesNeeded = Math.ceil(produceQty / reaction.quantity);
     const leftOver = timesNeeded * reaction.quantity - produceQty;
-    const result = (() => {
-      let total = 0;
-      for (let i = 0; i < timesNeeded; i++) {
-        total += reaction.needs.reduce(
-          (total, part) =>
-            total + getOreNeeded(part.chemical, part.quantity, leftOvers),
-          0
-        );
-      }
-      return total;
-    })();
+    const result = reaction.needs.reduce(
+      (total, part) =>
+        total +
+        getOreNeeded(part.chemical, timesNeeded * part.quantity, leftOvers),
+      0
+    );
 
     if (leftOverQty < quantity) {
       leftOvers.set(chemical, leftOver);
@@ -102,20 +92,24 @@ const solution2 = (inputLines: string[]) => {
     return result;
   };
 
-  let totalOreNeeded = 0;
   const target = 1000000000000;
+  const costForOne = getOreNeeded('FUEL', 1);
+  let minimum = costForOne;
+  let maximum = Number.POSITIVE_INFINITY;
 
-  const leftOvers = new Map();
-  let count = 0;
-  while (totalOreNeeded < target) {
-    const oreNeeded = getOreNeeded('FUEL', 1, leftOvers);
-    totalOreNeeded += oreNeeded;
-    count++;
-    console.log(totalOreNeeded / count);
+  while (minimum < maximum - 1) {
+    const test = Number.isFinite(maximum)
+      ? Math.ceil((minimum + maximum) / 2)
+      : minimum * 2;
+    const result = getOreNeeded('FUEL', test);
+    if (result <= target) {
+      minimum = test;
+    } else {
+      maximum = test;
+    }
   }
-  // By logging the average and waiting it to converge to 280199.3, I could guess that the number was 1000000000000 / 280199.3
 
-  return count - 1;
+  return minimum;
 };
 
 export {solution1, solution2};
