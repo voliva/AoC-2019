e958bcb7fcf9cbb73f2c9f7da832e29a6fa809ad
Víctor Oliva <olivarra1@gmail.com>
2019-12-16 07:44:17 +0100

    Solution to day 16

--- src/16/solution.ts
+++ src/16/solution.ts
@@ -6,8 +6,9 @@ const solution1 = (inputLines: string[]) => {
     .join('');
 };
 
-const FFT = (numbers: number[]) => {
+const FFT = (numbers: number[], repeat: number = 1) => {
   for (let n = 0; n < 100; n++) {
+    console.log(n);
     const accumulated = numbers.reduce((arr, num, i) => {
       if (i === 0) {
         arr[0] = num;
@@ -17,6 +18,13 @@ const FFT = (numbers: number[]) => {
       return arr;
     }, new Array(numbers.length));
 
+    const size = accumulated.length * repeat;
+    const getValue = (idx: number) => {
+      const looped = Math.floor(idx / accumulated.length);
+      const loopValue = accumulated[accumulated.length - 1];
+      return loopValue * looped + accumulated[idx % accumulated.length];
+    };
+
     numbers = numbers.map((_, idx) => {
       // Assuming pattern 0, 1, 0, -1
       // ...a...b.......c...d => (b-a)+(c-d)
@@ -24,10 +32,10 @@ const FFT = (numbers: number[]) => {
       let sign = 1;
       let start = idx - 1;
       let acc = 0;
-      while (start < accumulated.length - 1) {
-        const next = Math.min(start + idx + 1, accumulated.length - 1);
-        const a = start < 0 ? 0 : accumulated[start];
-        const b = accumulated[next];
+      while (start < size - 1) {
+        const next = Math.min(start + idx + 1, size - 1);
+        const a = start < 0 ? 0 : getValue(start);
+        const b = getValue(next);
         acc += sign * (b - a);
 
         sign *= -1;
@@ -41,7 +49,16 @@ const FFT = (numbers: number[]) => {
 };
 
 const solution2 = (inputLines: string[]) => {
-  return inputLines.length;
+  let input = '';
+  for (let i = 0; i < 10000; i++) {
+    input += inputLines[0];
+  }
+  const numbers = input.split('').map(Number);
+
+  const result = FFT(numbers);
+  const offset = Number(numbers.slice(0, 7).join(''));
+
+  return result.slice(offset, offset + 8).join('');
 };
 
 export {solution1, solution2};
