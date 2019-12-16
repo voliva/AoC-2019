2003252e3577f059114fee764fb10938264d6d6c
Víctor Oliva <olivarra1@gmail.com>
2019-12-16 06:42:37 +0100

    Efficient solution to part 1 day 16

--- src/16/solution.ts
+++ src/16/solution.ts
@@ -1,29 +1,43 @@
 const solution1 = (inputLines: string[]) => {
-  let numbers = inputLines[0].split('').map(Number);
-  const pattern = [0, 1, 0, -1];
+  const numbers = inputLines[0].split('').map(Number);
 
-  for (let n = 0; n < 10000; n++) {
-    numbers = numbers.map((_, idx) => {
-      let phase = 0;
-      let i = 1;
-      if (i > idx) {
-        phase = (phase + 1) % pattern.length;
-        i = 0;
+  return FFT(numbers)
+    .slice(0, 8)
+    .join('');
+};
+
+const FFT = (numbers: number[]) => {
+  for (let n = 0; n < 100; n++) {
+    const accumulated = numbers.reduce((arr, num, i) => {
+      if (i === 0) {
+        arr[0] = num;
+      } else {
+        arr[i] = arr[i - 1] + num;
       }
-      const raw = numbers.reduce((total, num) => {
-        const base = pattern[phase];
-        i++;
-        if (i > idx) {
-          phase = (phase + 1) % pattern.length;
-          i = 0;
-        }
-        return total + base * num;
-      }, 0);
-      return Math.abs(raw) % 10;
+      return arr;
+    }, new Array(numbers.length));
+
+    numbers = numbers.map((_, idx) => {
+      // Assuming pattern 0, 1, 0, -1
+      // ...a...b.......c...d => (b-a)+(c-d)
+
+      let sign = 1;
+      let start = idx - 1;
+      let acc = 0;
+      while (start < accumulated.length - 1) {
+        const next = Math.min(start + idx + 1, accumulated.length - 1);
+        const a = start < 0 ? 0 : accumulated[start];
+        const b = accumulated[next];
+        acc += sign * (b - a);
+
+        sign *= -1;
+        start += (idx + 1) * 2;
+      }
+
+      return Math.abs(acc) % 10;
     });
   }
-
-  return numbers.slice(0, 8).join('');
+  return numbers;
 };
 
 const solution2 = (inputLines: string[]) => {
