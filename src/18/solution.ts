e9e15c47252c96493ea1cecfa7713a7ea379d838
Víctor Oliva <olivarra1@gmail.com>
2019-12-20 16:48:48 +0100

    Back to recursive

--- src/18/solution.ts
+++ src/18/solution.ts
@@ -12,7 +12,6 @@ interface Distance {
   steps: number;
   doors: string[];
 }
-declare const process;
 const solution1 = (inputLines: string[]) => {
   const map = readInput(inputLines);
   const startRow = map.findIndex(line => line.includes('@'));
@@ -28,244 +27,22 @@ const solution1 = (inputLines: string[]) => {
     return distances;
   }, new Map<string, Map<string, Distance>>());
 
-  let distancesCache: Record<string, Record<string, number>> = {};
-  for (let [start, distances] of keyDistances) {
-    for (let [end, distance] of distances) {
-      if (start === end) {
-        continue;
-      }
-      if (distance.doors.includes(end.toUpperCase())) {
-        continue;
-      }
-      const key = [start, end].sort().join(',');
-      distancesCache[key] = distancesCache[key] || {};
-      distancesCache[key][start] = distance.steps;
-    }
-  }
-
-  const getFlatCache = (cache: Record<string, Record<string, number>>) => {
-    const totalSize = Object.keys(cache).reduce(
-      (total, key) => total + Object.keys(cache[key]).length,
-      0
-    );
-    const flatCache = new Array<[number, string, string]>(totalSize);
-    let i=0;
-    Object.keys(cache).forEach(combi => {
-      Object.keys(cache[combi]).forEach(start => {
-        flatCache[i] = [cache[combi][start], combi, start];
-        i++;
-      });
-    });
-    return flatCache;
-  };
-
-  for (let n = 2; n < allKeys.length; n++) {
-    const combinations = Object.keys(distancesCache);
-    const distancesCacheKeys = mapValues(distancesCache, c => Object.keys(c));
-    const newCache: Record<string, Record<string, number>> = {};
-
-    allKeys.forEach((origin, i) => {
-      console.log(`${n}/${allKeys.length} ${i}/${allKeys.length}`);
-      const distances = keyDistances.get(origin)!;
-
-      combinations.forEach((combi, j) => {
-        if(n >= 8 && n <= 16 && j % 100000 === 0) {
-          console.log(`${n}/${allKeys.length} ${i}/${allKeys.length} ${j}/${combinations.length}`);
-        }
-
-        if (combi.includes(origin)) {
-          return;
-        }
-        let minimum = Number.POSITIVE_INFINITY;
-        distancesCacheKeys[combi].forEach(start => {
-          const distance = distances.get(start)!;
-          if (distance.doors.some(door => combi.includes(door.toLowerCase()))) {
-            return;
-          }
-          const totalDistance = distance.steps + distancesCache[combi][start];
-          minimum = Math.min(minimum, totalDistance);
-        });
-        if (minimum > 6400) {
-          return;
-        }
-
-        const newCombi = combi
-          .split(',')
-          .concat([origin])
-          .sort()
-          .join(',');
-        newCache[newCombi] = newCache[newCombi] || {};
-        newCache[newCombi][origin] = minimum;
-      });
-    });
-    
-    // const flatCache = getFlatCache(newCache);
-    // if(flatCache.length > maxCacheSize) {
-    //   flatCache.sort((a,b) => b[0] - a[0]);
-    //   for(let i=0; i<flatCache.length-maxCacheSize; i++) {
-    //     const [_, combi, start] = flatCache[i];
-    //     delete newCache[combi][start];
-    //   }
-    //   console.log(flatCache.length, 'reduced', flatCache[0], flatCache[flatCache.length-maxCacheSize-1]);
-    // }
-
-    distancesCache = newCache;
-  }
-
-  console.log(distancesCache);
-  const resultDistance = Object.values(distancesCache)[0];
-
-  let minimum = Number.POSITIVE_INFINITY;
-  allKeys.forEach(key => {
-    const distance = originDistances.get(key)!;
-    if (distance.doors.length > 0) {
-      return;
-    }
-    minimum = Math.min(minimum, distance.steps + resultDistance[key]);
-  }, []);
-
-  return minimum;
-};
-
-const getAllGroups = <T>(array: Array<T>, n: number): Array<Array<T>> => {
-  if (n >= array.length) {
-    return [array];
-  }
-
-  const incrementKey = (keyArr: Array<number>, arrayLength = keyArr.length) => {
-    keyArr[arrayLength - 1]++;
-    if (keyArr[arrayLength - 1] >= array.length) {
-      if (arrayLength === 1) {
-        return false;
-      }
-      while (
-        incrementKey(keyArr, arrayLength - 1) &&
-        keyArr[arrayLength - 2] + 1 >= array.length
-      );
-      keyArr[arrayLength - 1] = keyArr[arrayLength - 2] + 1;
-      if (keyArr[arrayLength - 1] >= array.length) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const result = new Array<Array<T>>();
-  const keys = new Array(n).fill(0).map((_, i) => i);
-  do {
-    result.push(keys.map(k => array[k]));
-  } while (incrementKey(keys));
-
-  return result;
-};
-
-const getAllStartingGroups = <T>(
-  array: Array<T>,
-  n: number
-): Array<Array<T>> => {
-  let result: Array<Array<T>> = [];
-  array.forEach(
-    value =>
-      (result = result.concat(
-        getAllGroups(
-          array.filter(v => v !== value),
-          n - 1
-        ).map(group => [value, ...group])
-      ))
+  // It's not 5976
+  return recursiveSolution(
+    allKeys,
+    [],
+    originDistances,
+    keyDistances,
+    {}
   );
-
-  return result;
-};
-
-// Priority queue where I'm always proceeding on the one with the less steps
-const iterativeSolution = (
-  missingKeys: string[],
-  originDistances: Map<string, Distance>,
-  allDistances: Map<string, Map<string, Distance>>,
-  distancesCache: Map<string, number>
-) => {
-  const reachableKeys = missingKeys.filter(key => {
-    const {doors} = originDistances.get(key)!;
-    return (
-      doors.filter(door => missingKeys.includes(door.toLowerCase())).length ===
-      0
-    );
-  });
-
-  if (!reachableKeys.length) {
-    return Number.POSITIVE_INFINITY;
-  }
-
-  // [steps, [keys]]
-  const positionsToVisit = new TinyQueue<[number, string, string[]]>(
-    reachableKeys.map(key => {
-      const {steps} = originDistances.get(key)!;
-      return [steps, key, [key]];
-    }),
-    (a, b) => b[0] - a[0]
-  );
-
-  let minSteps = Number.POSITIVE_INFINITY;
-  while (positionsToVisit.length) {
-    const [steps, currentKey, keysUnlocked] = positionsToVisit.pop()!;
-    if (steps > minSteps) {
-      continue;
-    }
-
-    const stepMissingKeys = missingKeys.filter(
-      key => !keysUnlocked.includes(key)
-    );
-    const distanceCacheKey = [currentKey, ...stepMissingKeys.sort()].join(',');
-
-    if (distancesCache.has(distanceCacheKey)) {
-      const totalSteps = steps + distancesCache.get(distanceCacheKey)!;
-      if (totalSteps < minSteps) {
-        // console.log(minSteps);
-        minSteps = steps;
-      }
-      continue;
-    }
-
-    const distances = allDistances.get(currentKey)!;
-
-    if (keysUnlocked.length === missingKeys.length) {
-      if (steps < minSteps) {
-        // console.log(steps);
-        minSteps = steps;
-      }
-      continue;
-    }
-
-    const reachableKeys = missingKeys.filter(key => {
-      if (keysUnlocked.includes(key)) {
-        return false;
-      }
-      const {doors} = distances.get(key)!;
-      return doors
-        .filter(door => missingKeys.includes(door.toLowerCase()))
-        .every(door => keysUnlocked.includes(door.toLowerCase()));
-    });
-
-    reachableKeys.forEach(key => {
-      const distance = distances.get(key)!;
-      positionsToVisit.push([
-        steps + distance.steps,
-        key,
-        [...keysUnlocked, key],
-      ]);
-    });
-  }
-
-  return minSteps;
 };
 
 const recursiveSolution = (
-  maximumSteps: number,
   keysMissing: string[],
   keysCollected: string[],
   distances: Map<string, Distance>,
   allDistances: Map<string, Map<string, Distance>>,
-  log: number = 0
+  cache: Record<string, Record<string, number>>
 ): number => {
   if (keysMissing.length === 0) {
     return 0;
@@ -280,35 +57,27 @@ const recursiveSolution = (
     return Number.POSITIVE_INFINITY;
   }
 
-  const sortedRK = reachableKeys.sort((a, b) => {
-    return distances.get(a)!.steps - distances.get(b)!.steps;
+  let minimum:number = Number.POSITIVE_INFINITY;
+  reachableKeys.forEach(key => {
+    const {steps} = distances.get(key)!;
+
+    const newKeysMissing = keysMissing.filter(k => k !== key);
+    const cacheKey = newKeysMissing.join(',');
+    cache[cacheKey] = cache[cacheKey] || {};
+
+    const subTreeSteps = key in cache[cacheKey] ? cache[cacheKey][key] : recursiveSolution(
+      newKeysMissing,
+      keysCollected.concat(key),
+      allDistances.get(key)!,
+      allDistances,
+      cache
+    );
+    cache[cacheKey][key] = subTreeSteps;
+
+    minimum = Math.min(minimum, steps + subTreeSteps);
   });
 
-  const steps = sortedRK
-    .slice(0, Math.ceil((6 * sortedRK.length) / 12))
-    .map((key, i) => {
-      if (log > 0) {
-        console.log(log, key, i, reachableKeys.length);
-      }
-      const {steps} = distances.get(key)!;
-      if (steps > maximumSteps) {
-        return Number.POSITIVE_INFINITY;
-      }
-
-      return (
-        steps +
-        recursiveSolution(
-          maximumSteps - steps,
-          keysMissing.filter(k => k !== key),
-          keysCollected.concat(key),
-          allDistances.get(key)!,
-          allDistances,
-          log - 1
-        )
-      );
-    });
-
-  return steps.reduce((a, b) => Math.min(a, b));
+  return minimum;
 };
 
 const keyRegex = /^[a-z]$/;
@@ -368,7 +137,7 @@ const countSteps = (map: string[][], from: number[], keys: string[]) => {
     }
 
     if (doorRegex.test(char)) {
-      doors = [...doors, char];
+      doors = [...doors, char.toLowerCase()];
     }
 
     const isWall = char === '#';
